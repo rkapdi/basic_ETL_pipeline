795a7a254641f755465dc84dfb083fefb3d117d5
Rissalat Kapdi <rkapdi@Rissalats-MacBook-Pro.local>
2023-05-24 15:49:45 -0700

    flask setup

---
+++ ETL.py
@@ -0,0 +1,13 @@
+import requests
+import pandas as pd
+
+def get_human_characters():
+    response = requests.get('https://rickandmortyapi.com/api/character')
+    data = response.json()
+    characters = data['results']
+
+    human_characters = [character for character in characters if character['species'] == 'Human']
+
+    df = pd.DataFrame(human_characters)
+    html_table = df.to_html()
+    return html_table
